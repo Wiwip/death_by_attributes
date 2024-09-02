ff6e3bd6b4d0a22a0e1400e988a547a7999d2e24
LP <lpafortier@gmail.com>
2024-09-02 09:41:26 -0400

    Simple macro to define easy attributes.

--- src/attributes.rs
+++ src/attributes.rs
@@ -7,3 +7,13 @@ pub struct GameAttribute {
     pub base_value: f32,
     pub current_value: f32,
 }
+
+#[macro_export]
+macro_rules! easy_attribute {
+    ( $StructName:ident) => {
+        #[derive(Component, Attribute, Reflect, Deref, DerefMut)]
+        struct $StructName {
+            pub value: GameAttribute,
+        }
+    };
+}
